8f0b18eb6e729778346464ddf5adb62b148f164a
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-04-05 23:16:41 +0600

    🛠️📕💅🏷️updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import AppSidebar from "./Views/AppSidebar";
+import ContentContainer from "./Components/ContentContainer";
+import FrequencyTable from "./Views/FrequencyTable";
+import { process } from "./Functions/processData";
+import HistoGram from "./Views/HistoGram";
 
-function App() {
+export default function App() {
+  const [DATA, setDATA] = useState();
+  const [ProcessedDATA, setProcessedDATA] = useState();
+
+  const NAVs = [
+    // {
+    //   id: 1,
+    //   name: "Identify Data Type",
+    //   component: "",
+    // },
+    {
+      name: "Frequency Table",
+      component: <FrequencyTable data={ProcessedDATA} />,
+    },
+    {
+      name: "Histogram",
+      component: <HistoGram data={ProcessedDATA} />,
+    },
+    {
+      name: "Arithmetic Mean",
+      component: <HistoGram data={ProcessedDATA} />,
+    },
+    {
+      name: "Median",
+      component: <HistoGram data={ProcessedDATA} />,
+    },
+    {
+      name: "Mode",
+      component: <HistoGram data={ProcessedDATA} />,
+    },
+    {
+      name: "GeoMetric Mean",
+      component: <HistoGram data={ProcessedDATA} />,
+    },
+    {
+      name: "Harmonic Mean",
+      component: <HistoGram data={ProcessedDATA} />,
+    },
+  ];
+
+  const [ActiveNav, setActiveNav] = useState(0);
+
+  useEffect(() => {
+    setProcessedDATA(DATA && process(DATA));
+  }, [DATA]);
+  // console.log(ProcessedDATA);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <AppSidebar navArray={NAVs} cNav={ActiveNav} setCNav={setActiveNav} />
+      <ContentContainer tab={ActiveNav} pushData={setDATA} navArray={NAVs}>
+        {NAVs[ActiveNav].component}
+      </ContentContainer>
+    </Container>
   );
 }
 
-export default App;
+const Container = styled.div`
+  width: min(1440px, 100%);
+  height: min(100dvh, 100vh);
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 3fr 9fr;
+`;
