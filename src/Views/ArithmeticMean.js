7221a8d47f456ee34257de3fb51a20c8a7b6603d
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-04-06 00:33:09 +0600

    🛠️📕💅🏷️added and done v-alpha

---
+++ src/Views/ArithmeticMean.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+import ArithmeticMeanComponent from "../Components/ArithmeticMeanComponent";
+
+export default function ArithmeticMean({ data }) {
+  return (
+    <Container>
+      <h1>Arithmetic Mean{data.length > 1 && "s"}:</h1>
+      {data &&
+        Array.isArray(data) &&
+        data.length > 0 &&
+        React.Children.toArray(
+          data.map((el) => (
+            <ArithmeticMeanComponent data={el.data} title={el.name} />
+          ))
+        )}
+    </Container>
+  );
+}
+
+const Container = styled.main`
+  /* padding: 24px 0; */
+`;
