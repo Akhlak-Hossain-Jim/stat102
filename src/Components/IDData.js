25f5e8f166fb9b39986cbca3c8020e737318c10c
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-04-05 23:18:13 +0600

    💅🏷️added

---
+++ src/Components/IDData.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function IDData() {
+  return <Container>IDData</Container>;
+}
+
+const Container = styled.main`
+  padding: 24px;
+`;
