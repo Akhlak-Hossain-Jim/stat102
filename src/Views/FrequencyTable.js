a53e93ad45afc4db9e7beeee0e830e1998cf6525
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-04-12 11:32:29 +0600

    📕title added

--- src/Views/FrequencyTable.js
+++ src/Views/FrequencyTable.js
@@ -5,6 +5,7 @@ import FrequencyTableComponent from "../Components/FrequencyTableComponent";
 export default function FrequencyTable({ data }) {
   return (
     <Container>
+      <h1>Frequency Table{data && data.length > 1 && "s"}:</h1>
       {data &&
         Array.isArray(data) &&
         data.length > 0 &&
--- src/Views/HistoGram.js
+++ src/Views/HistoGram.js
@@ -5,6 +5,7 @@ import HistogramComponent from "../Components/HistogramComponent";
 export default function HistoGram({ data }) {
   return (
     <Container>
+      <h1>Graphical Presentation{data && data.length > 1 && "s"}:</h1>
       {data &&
         Array.isArray(data) &&
         data.length > 0 &&
