52abdebcd98611121c5920ba101e7bbfc04816e3
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-05-12 17:08:10 +0600

    🛠️📕💅🏷️added and done v-alpha

---
+++ src/Components/MeanDeviation.js
@@ -0,0 +1,251 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { dataIsNumber } from "../Functions/processData";
+import { MeanDeviationCalc } from "../Functions/dispersionMath";
+import { BsCircle, BsRecordCircle } from "react-icons/bs";
+import Divide from "./Divide";
+
+function Content({ title, data }) {
+  const [isNum, setIsNum] = useState();
+  const [MeanDevObj, setMeanDevObj] = useState();
+
+  const [NType, setNType] = useState("sample");
+
+  const NTypes = [
+    {
+      label: "Population",
+      value: "population",
+      details: "The given data has information for all people/item there is.",
+    },
+    {
+      label: "Sample",
+      value: "sample",
+      details:
+        "The given data has information of some of all people/item there is.",
+    },
+  ];
+
+  useEffect(() => {
+    setIsNum(dataIsNumber(data));
+  }, [data]);
+
+  useEffect(() => {
+    if (isNum) {
+      setMeanDevObj(MeanDeviationCalc(data));
+    }
+  }, [data, isNum]);
+
+  console.log(MeanDevObj);
+
+  return (
+    <ContentContainer>
+      {isNum && MeanDevObj ? (
+        <>
+          <h1 className="title">Range of {title && title}:</h1>
+          <div className="am_con">
+            <div className="choose_nType">
+              <h2>Choose variable type:</h2>
+              {React.Children.toArray(
+                NTypes.map((nt) => (
+                  <div
+                    className="option"
+                    title={nt.details}
+                    onClick={() => setNType(nt.value)}
+                    role="button"
+                  >
+                    {nt.value === NType ? <BsRecordCircle /> : <BsCircle />}
+                    {nt.label}
+                  </div>
+                ))
+              )}
+            </div>
+            <div className="calculation">
+              {NType === "sample" ? (
+                <div className="math">
+                  <var>
+                    Mean Deviation ={" "}
+                    <Divide a={<>&sum; | x - x_bar |</>} b={"n"} />
+                  </var>
+                  <var>
+                    Mean Deviation ={" "}
+                    <Divide
+                      a={React.Children.toArray(
+                        data.map((vx, i) => (
+                          <>
+                            | {vx} - {MeanDevObj.avg.toFixed(2)} |{" "}
+                            {i !== data.length - 1 && " + "}
+                          </>
+                        ))
+                      )}
+                      b={data.length}
+                    />
+                  </var>
+                  <var>
+                    Mean Deviation ={" "}
+                    <Divide
+                      a={React.Children.toArray(
+                        MeanDevObj.arr2.map((val, i) => (
+                          <>
+                            {val.toFixed(2)}
+                            {i !== MeanDevObj.arr2.length - 1 && " + "}
+                          </>
+                        ))
+                      )}
+                      b={data.length}
+                    />
+                  </var>
+                  <var>
+                    Mean Deviation ={" "}
+                    <Divide
+                      a={MeanDevObj.sumOfDiff.toFixed(2)}
+                      b={data.length}
+                    />
+                  </var>
+                  <var>Mean Deviation = {MeanDevObj.res.toFixed(2)}</var>
+                </div>
+              ) : NType === "population" ? (
+                <div className="math">
+                  <var>
+                    Mean Deviation ={" "}
+                    <Divide a={<>&sum; | x - &micro; |</>} b={"N"} />
+                  </var>
+                  <var>
+                    Mean Deviation ={" "}
+                    <Divide
+                      a={React.Children.toArray(
+                        data.map((vx, i) => (
+                          <>
+                            | {vx} - {MeanDevObj.avg.toFixed(2)} |{" "}
+                            {i !== data.length - 1 && " + "}
+                          </>
+                        ))
+                      )}
+                      b={data.length}
+                    />
+                  </var>
+                  <var>
+                    Mean Deviation ={" "}
+                    <Divide
+                      a={React.Children.toArray(
+                        MeanDevObj.arr2.map((val, i) => (
+                          <>
+                            {val.toFixed(2)}
+                            {i !== MeanDevObj.arr2.length - 1 && " + "}
+                          </>
+                        ))
+                      )}
+                      b={data.length}
+                    />
+                  </var>
+                  <var>
+                    Mean Deviation ={" "}
+                    <Divide
+                      a={MeanDevObj.sumOfDiff.toFixed(2)}
+                      b={data.length}
+                    />
+                  </var>
+                  <var>Mean Deviation = {MeanDevObj.res.toFixed(2)}</var>
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+            <p>
+              <u>Comment:</u> The average absolute distance from mean of {title}{" "}
+              data set is {MeanDevObj.res.toFixed(2)}.
+            </p>
+          </div>
+        </>
+      ) : (
+        <h1 className="comment">
+          Can not perform <strong>Mean Deviation</strong> calculation on "
+          {title}" data set, as the data is not numerical or categorical data
+        </h1>
+      )}
+    </ContentContainer>
+  );
+}
+const ContentContainer = styled.div`
+  padding: 24px 0;
+  display: flex;
+  flex-direction: column;
+  & > h1 {
+    font-size: 1.4rem;
+    font-weight: 500;
+    padding: 12px 0;
+    color: var(--light-green);
+    &.title {
+      margin: 12px 0;
+      padding: 6px 0;
+      display: inline;
+      width: max-content;
+      border-bottom: 1px dashed var(--green);
+    }
+    & > strong {
+      text-decoration: underline;
+    }
+  }
+  & > .am_con {
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    & > .choose_nType {
+      display: flex;
+      flex-direction: column;
+      gap: 16px;
+      & > h2 {
+        color: var(--light-green);
+        font-size: 1.2rem;
+        font-weight: 500;
+      }
+      & > .option {
+        display: flex;
+        align-items: center;
+        gap: 12px;
+        cursor: pointer;
+      }
+    }
+    & > .calculation {
+      display: flex;
+      flex-direction: column;
+      gap: 12px;
+      & > .math {
+        display: flex;
+        gap: 8px;
+        flex-direction: column;
+        & > var {
+          display: flex;
+          align-items: center;
+          gap: 8px;
+          white-space: nowrap;
+          & > * {
+            white-space: break-spaces;
+          }
+        }
+      }
+    }
+    & > .note {
+      & > ul {
+        padding: 12px 32px;
+      }
+    }
+  }
+`;
+export default function MeanDeviation({ data }) {
+  return (
+    <Container>
+      <h1>
+        Mean Deviation{data && data.length > 1 ? "s" : ""} (Absolute
+        Dispersion):
+      </h1>
+      {data &&
+        Array.isArray(data) &&
+        data.length > 0 &&
+        React.Children.toArray(
+          data.map((el) => <Content data={el.data} title={el.name} />)
+        )}
+    </Container>
+  );
+}
+
+const Container = styled.main``;
---
+++ src/Functions/dispersionMath.js
@@ -0,0 +1,27 @@
+import { dataIsNumber } from "./processData";
+
+export function RangeCalc(data) {
+  let res;
+  let min;
+  let max;
+  if (dataIsNumber(data)) {
+    let a = data.map((el) => Number(el)).sort((a, b) => a - b);
+    min = a[0];
+    max = a[a.length - 1];
+    res = max - min;
+  }
+  return { range: res, min, max };
+}
+
+export function MeanDeviationCalc(data) {
+  if (dataIsNumber(data)) {
+    let arr = [...data].map((el) => Number(el));
+    let n = arr.length;
+    let sum = arr.reduce((a, b) => a + b, 0);
+    let avg = sum / n;
+    let arr2 = [...arr].map((el) => Math.abs(el - avg));
+    let sum2 = arr2.reduce((a, b) => a + b, 0);
+    let res = sum2 / n;
+    return { sumOfDiff: sum2, res, avg, arr2 };
+  }
+}
