606490c47eccc57278156a9a7e588eee98801084
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-05-15 01:13:59 +0600

    🛠️📕💅🏷️added and done v-alpha

---
+++ src/Functions/PercentMaths.js
@@ -0,0 +1,29 @@
+export function percentOf(minMax, per) {
+  if (
+    minMax.min &&
+    minMax.max &&
+    !isNaN(minMax.min) &&
+    !isNaN(minMax.max) &&
+    per &&
+    !isNaN(per)
+  ) {
+    let range = minMax.max - minMax.min;
+    let res = minMax.min + range * (per / 100);
+    return res;
+  }
+}
+export function percentOfNumInRange(minMax, num) {
+  if (
+    minMax.min &&
+    minMax.max &&
+    !isNaN(minMax.min) &&
+    !isNaN(minMax.max) &&
+    num &&
+    !isNaN(num)
+  ) {
+    let range = minMax.max - minMax.min;
+    let diff = num - minMax.min;
+    let res = (diff / range) * 100;
+    return res;
+  }
+}
