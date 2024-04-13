3bb0ad80294fa1668ddd9b14f2f222ad21e4bfe8
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-04-13 18:22:15 +0600

    📕🛠️💅update

--- src/Components/ArithmeticMeanComponent.js
+++ src/Components/ArithmeticMeanComponent.js
@@ -153,7 +153,7 @@ const Container = styled.div`
       padding: 6px 0;
       display: inline;
       width: max-content;
-      border-bottom: 1px dashed var(--green);
+      border-bottom: 1px dashed var(--green-2);
     }
     & > strong {
       text-decoration: underline;
